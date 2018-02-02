8e821382409079c132eb5d62d4c19897e107de6b
John Evans <jakepevans@gmail.com>
2018-02-02 11:09:49 -0500

    first commit

---
+++ SignalFilteringTest/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace SignalFilteringTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CSV File Location";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(458, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(15, 48);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1106, 503);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(109, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(343, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "C:\\Users\\john\\Desktop\\noisyData.csv";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1133, 540);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "chart";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ SignalFilteringTest/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SignalFilteringTest {
+    public partial class Form1 : Form {
+
+        List<double> raw = new List<double>();
+        List<double> output1;
+        List<double> output2;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void init() {
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+        }
+
+        // load the file, parse the data out, run any filtering operations, then run charting ops
+        private void button1_Click(object sender, EventArgs e) {
+            if (!File.Exists(textBox1.Text)) {
+                MessageBox.Show("Did not find file " + textBox1.Text);
+                return;
+            }
+            try {
+                List<Int64> listA = new List<Int64>();
+                List<int> listB = new List<int>();
+                using (var reader = new StreamReader(textBox1.Text)) {
+
+                    while (!reader.EndOfStream) {
+                        var line = reader.ReadLine();
+
+                        if (line == "") {
+                            continue;
+                        }
+
+                        var values = line.Split(',');
+
+                        listA.Add(Int64.Parse(values[0]));
+                        raw.Add((double)int.Parse(values[1]));
+                    }
+                }
+                // do some signal filtering on the values:
+                MathNet.Filtering.OnlineFilter lowpass = MathNet.Filtering.OnlineFilter.CreateLowpass(MathNet.Filtering.ImpulseResponse.Finite, 20, 2,1);
+                MathNet.Filtering.OnlineFilter denoise = MathNet.Filtering.OnlineFilter.CreateDenoise(3);
+                output1 = denoise.ProcessSamples(raw.Select(a => (double)a).ToArray()).ToList();
+                output2 = lowpass.ProcessSamples(output1.Select(a => (double)a).ToArray()).ToList();
+
+                chart1.Series.Clear();
+
+                // graph the data
+                var series1 = new System.Windows.Forms.DataVisualization.Charting.Series {
+                    Name = "Series1",
+                    Color = System.Drawing.Color.Green,
+                    IsVisibleInLegend = false,
+                    IsXValueIndexed = false,
+                    ChartType = SeriesChartType.Line
+                };
+
+                this.chart1.Series.Add(series1);
+
+                int i = 0;
+                foreach(var point in raw.ToArray()) {
+                    series1.Points.AddXY(i, point);
+                    i++;
+                }
+
+                var series2 = new System.Windows.Forms.DataVisualization.Charting.Series {
+                    Name = "Series2",
+                    Color = System.Drawing.Color.Red,
+                    IsVisibleInLegend = false,
+                    IsXValueIndexed = false,
+                    ChartType = SeriesChartType.Line
+                };
+
+                this.chart1.Series.Add(series2);
+
+                int i2 = 0;
+                foreach (var point in output1.ToArray()) {
+                    series2.Points.AddXY(i2, point);
+                    i2++;
+                    if (i2 > i-1) {
+                        break;
+                    }
+                }
+
+
+                var series3 = new System.Windows.Forms.DataVisualization.Charting.Series {
+                    Name = "Series3",
+                    Color = System.Drawing.Color.Blue,
+                    IsVisibleInLegend = false,
+                    IsXValueIndexed = false,
+                    ChartType = SeriesChartType.Line
+                };
+
+                this.chart1.Series.Add(series3);
+
+                int i3 = 0;
+                foreach (var point in output2.ToArray()) {
+                    series3.Points.AddXY(i3, point);
+                    i3++;
+                    if (i3 > i-1) {
+                        break;
+                    }
+                }
+
+
+                chart1.Invalidate();
+                Application.DoEvents();
+            }
+            catch {
+                // fall through
+            }            
+        }
+        
+
+        static private void WriteFile(double[] noisy, double[] clean) {
+            using (TextWriter tw = new StreamWriter("data.csv")) {
+                for (int i = 0; i < noisy.Length; i++)
+                    tw.WriteLine(string.Format("{ 0:0.00}, { 1:0.00}", noisy[i], clean[i]));
+                tw.Close();
+            }
+        }
+
+        // not going to be used, but useful for testing perhaps
+        static private double[] NoisySine() {
+            // Create a noisy sine wave.
+            double[] noisySine = new double[180];
+            Random rnd = new Random();
+            for (int i = 0; i < 180; i++)
+                noisySine[i] = Math.Sin(Math.PI * i / 90) + rnd.NextDouble() - 0.5;
+            return noisySine;
+        }
+    }
+}
